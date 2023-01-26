760eab71d00aed08d8c2bc9b5e9de300c2fb65bf
Justin Shaffer <justinbshaffer91@gmail.com>
2023-01-26 10:13:26 -0800

    create langType function, Submit/Reset btn, add basic box styling

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+  const form = document.querySelector('form');
+  form.onsubmit = function(e) {
+    e.preventDefault();
+  }
+}
+
+function langType() {
+  let activity = document.querySelector('input[name="activity"]:checked').value;
+  let awareness = document.querySelector('input[name="awareness"]:checked').value;
+  let decide = document.querySelector('input[name="decide"]:checked').value;
+  let food = document.querySelector('input[name="food"]:checked').value;
+  let animal = document.querySelector('input[name="animal"]:checked').value;
+
+  let langType;
+  if (activity === "1" && awareness === "1" && decide === "1" && food === "1" && animal === "1") {
+    langType = "Javascript";
+  } else if (activity === "2" && awareness === "2" && decide === "2" && food === "2" && animal === "2") {
+    langType = "Ruby";
+  } else if (activity === "3" && awareness === "3" && decide === "3" && food === "3" && animal === "3") {
+    langType = "Python";
+  } else {
+    langType = "C#";
+  }
+
+  const resultDiv = document.getElementById("result");
+  resultDiv.textContent = "We recommend learning " + langType + " as your first programming language.";
+}
+
+const tryAgainBtn = document.getElementById("tryAgain");
+tryAgainBtn.addEventListener("click", function() {
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].checked = false;
+  }
+  var resultDiv = document.getElementById("result");
+  resultDiv.textContent = "";
+});
